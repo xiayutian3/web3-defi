16abbd4c2e476ee9871c8969f2d1d8894379beec
1127071993@qq.com <aini2009626>
2023-02-26 20:36:54 +0800

    020-web3的高频API-账号创建

---
+++ 017钱包开发搭建环境/web3-wallet/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ str }}</h1>
+  <van-button type="primary">12345</van-button>
+  <web3Api></web3Api>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Web3 from "web3";
+import web3Api from "./components/Web3Api.vue";
+
+const str = ref("hello123");
+
+// 实例化web3
+// eslint-disable-next-line no-unused-vars
+var web3 = new Web3(
+  Web3.givenProvider ||
+    // 在这里获取测试网 goerli地址  https://app.infura.io/dashboard/ethereum/ebace207d26e4e489a31dc073732b8cc/settings
+    "wss://goerli.infura.io/ws/v3/ebace207d26e4e489a31dc073732b8cc"
+);
+// console.log("web3: ", web3);
+</script>
+
+<style lang="scss"></style>
---
+++ 017钱包开发搭建环境/web3-wallet/src/components/Web3Api.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>123</div>
+</template>
+
+<script setup>
+import Web3 from "web3";
+
+// 实例化web3
+var web3 = new Web3(
+  Web3.givenProvider ||
+    // 在这里获取测试网 goerli地址  https://app.infura.io/dashboard/ethereum/ebace207d26e4e489a31dc073732b8cc/settings
+    "wss://goerli.infura.io/ws/v3/ebace207d26e4e489a31dc073732b8cc"
+);
+// console.log("web3: ", web3);
+
+// 创建账户
+const newAccount = web3.eth.accounts.create("123");
+console.log("newAccount: ", newAccount);
+</script>
+
+<style lang="scss"></style>
